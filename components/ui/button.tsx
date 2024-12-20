02c87fcc27561ccacce8b2b3aac7addd770a650a
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-12-20 09:34:20 +0100

    Landing Page

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,6 @@
-// @ts-ignore
-export default function Button({ children, className }) {
+import React from "react";
+
+export default function Button({ children, className }: {children: React.ReactNode, className: string}) {
     const baseStyles =
         "inline-flex items-center justify-center font-medium rounded-md transition-all focus:outline-none focus:ring-2 focus:ring-offset-2";
     
